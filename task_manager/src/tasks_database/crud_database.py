efd7f3facefb13946c755f7239bea3642a7edb8c
Davi <davipare90@gmail.com>
2024-09-04 01:14:00 -0300

    edit models to accept dependencies

--- task_manager/src/tasks_database/crud_database.py
+++ task_manager/src/tasks_database/crud_database.py
@@ -18,8 +18,8 @@ if DATABASE_URL is None:
 
 try:
     engine = create_engine(DATABASE_URL)
-    LocalSession = sessionmaker(autocommit=False, 
-                                autoflush=False, 
+    LocalSession = sessionmaker(autocommit=False,
+                                autoflush=False,
                                 bind=engine)
 except Exception as e:
     raise ValueError(f"Failed to create engine: {e}")
@@ -45,7 +45,10 @@ def quick_query(obj: Any, filter_dict: Dict[str, Any]):
         return query
 
 
-def create_task(name: str, description: Optional[str], creation_date: str, status: str):
+def create_task(name: str,
+                description: Optional[str],
+                creation_date: str,
+                status: str):
     with get_db() as db:
         if (
             quick_query(Task, {"name": name}) is not None
@@ -68,7 +71,6 @@ def create_task(name: str, description: Optional[str], creation_date: str, statu
 
 def delete_all_updates_from_task(task_id):
     with get_db() as db:
-        # updates_to_delete = db.query(Updates).filter(Updates.task_id == task_id)
         delete_stmt = delete(Updates).where(Updates.task_id == task_id)
         db.execute(delete_stmt)
         db.commit()
--- task_manager/src/updates_manager.py
+++ task_manager/src/updates_manager.py
@@ -3,6 +3,7 @@ from datetime import date
 from .tasks_storage import InvalidOperationError
 from typing import Dict, Union
 
+
 @dataclass
 class Update:
     _description: str
@@ -12,28 +13,30 @@ class Update:
     @property
     def description(self):
         return self._description
-    
+
     @property
     def creation_date(self):
         return self._creation_date
-    
+
     @creation_date.setter
     def creation_date(self):
         raise InvalidOperationError('Creation date can\'t be changed.')
+
 
 class UpdatesHandler:
     # Syntax: {task_id: { update_id: Update }}
     def __init__(self):
         self.updates: Dict[int, Dict[int, Update]] = {}
 
-    def __gen_new_update_id(self, task_id:int) -> int:
+    def __gen_new_update_id(self, task_id: int) -> int | None:
         try:
             update_ids = self.updates[task_id].keys()
             return int(len(update_ids))+1
 
         except Exception as e:
             print(f"Error while generating a new update id: {e}")
-    
+            return
+
     def get_last_update(self, task_id):
         try:
             last_id: int = max(self.updates[task_id].keys())
@@ -41,14 +44,14 @@ class UpdatesHandler:
 
         except Exception as e:
             print(f'Error occurred while searching the last update: {e}')
-    
+
     def new_update(self, task_id: Union[str, int], description: str):
         if isinstance(task_id, str) and task_id.isdigit():
             task_id = int(task_id)
 
         elif isinstance(task_id, str) and not task_id.isdigit():
             raise ValueError("Task id is not a number.")
-        
+
         if task_id not in self.updates.keys():
             self.updates[task_id] = {}
 
